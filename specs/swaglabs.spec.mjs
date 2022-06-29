8898badf5f85dfd83784c9b8a8e69054e117f18a
plebedev <lebedev.p.861@gmail.com>
2022-06-29 13:37:41 +0300

    Локаторы для ожидаемых результатов перенес в elements

--- specs/swaglabs.spec.mjs
+++ specs/swaglabs.spec.mjs
@@ -3,29 +3,29 @@ import { test, expect } from '@playwright/test';
 
     test('Авторизоваться стандартным  пользователем', async ({page}) => {
         await app().Auth().login(page)                    
-        const welcomeText = await page.locator('text=Products');
+        const welcomeText = await app().assertloc().getLogin(page);
         await expect(welcomeText).toContainText('Products')
     });
     test('Разлогиниться стандартным  пользователем', async ({page}) => {
         await app().Auth().login(page)
         await app().ProductsPage().logout(page)
-        const locator = await page.locator("#login-button");
+        const locator =await app().assertloc().getLogOut(page);
         await expect(locator).toBeVisible()
     
     });
     test('Положить товар в корзину', async ({page}) => {
         await app().Auth().login(page)
         await app().ProductsPage().addProduct(page)        
-        const locator2 = await page.locator(".shopping_cart_badge")
-        await expect(locator2).toContainText('1')
+        const locator = await app().assertloc().getAddProduct(page)
+        await expect(locator).toContainText('1')
              });    
 
      test('Убрать товар из корзины', async ({page}) => {
         await app().Auth().login(page)
         await app().ProductsPage().addProduct(page)
         await app().ProductsPage().removeProduct(page)
-        const locator2 = await page.locator(".shopping_cart_badge")
-        await expect(locator2).toBeHidden()
+        const locator = await app().assertloc().getAddProduct(page)
+        await expect(locator).toBeHidden()
              });
 
     test('Совершить покупку', async ({page}) => {
@@ -33,8 +33,8 @@ import { test, expect } from '@playwright/test';
         await app().ProductsPage().addProduct(page)
         await app().ProductsPage().seeCart(page)
         await app().BuyProduct().buy(page)
-        const locator2 = await page.locator(".complete-header")
-        await expect(locator2).toContainText('THANK YOU FOR YOUR ORDER')
+        const locator = await app().assertloc().getBuyProduct(page)
+        await expect(locator).toContainText('THANK YOU FOR YOUR ORDER')
              });
 
      
